2e2d8a0d163d18469b00638ecf42bb11501b929d
gabriele11231 <gabriele_giorgio@virgilio.it>
2022-08-26 10:40:59 +0200

    Add part of degree analyses

---
+++ Network_Analyses/Degree.py
@@ -0,0 +1,87 @@
+from traceback import print_tb
+import networkx as nx
+import numpy as np
+
+#----DEGREE-ANALYSES-VIRAL----
+viral_dict = dict()
+mean_degree_l = list()
+max_country_l = list()
+max_degree_l = list()
+min_country_l = list()
+min_degree_l = list()
+for z in range(1, 16):
+
+    grafo = nx.read_gpickle(f'Dataset\\viral-pkl\\viral{z}.pkl')
+
+    degree = dict(grafo.degree(weight='weight'))
+    campione_grado = list(degree.values())
+    mean_degree_l.append(np.mean(list(degree.values())))
+
+
+    max_degree = max(degree.values())
+    max_country = ''
+    for k, v in grafo.degree(weight='weight'):
+        if v == max_degree:
+            max_country = k
+            break
+    max_country_l.append(grafo.nodes.data("label")[max_country])
+    max_degree_l.append(max_degree)
+
+
+    min_degree = min(degree.values())
+    min_country = ''
+    for k, v in grafo.degree(weight='weight'):
+        if v == min_degree:
+            min_country = k
+            break
+    min_country_l.append(grafo.nodes.data("label")[min_country])
+    min_degree_l.append(min_degree)
+
+viral_dict['mean_degree'] = mean_degree_l
+viral_dict['max_country'] = max_country_l
+viral_dict['max_degree'] = max_degree_l
+viral_dict['min_country'] = min_country_l
+viral_dict['min_degree'] = min_degree_l
+
+
+#----DEGREE-ANALYSES-GLOBAL----
+global_dict = dict()
+mean_degree_l = list()
+max_country_l = list()
+max_degree_l = list()
+min_country_l = list()
+min_degree_l = list()
+for z in range(1, 16):
+
+    grafo = nx.read_gpickle(f'Dataset\\global-pkl\\global{z}.pkl')
+
+    degree = dict(grafo.degree(weight='weight'))
+    campione_grado = list(degree.values())
+    mean_degree_l.append(np.mean(list(degree.values())))
+
+    max_degree = max(degree.values())
+    max_country = ''
+    for k, v in grafo.degree(weight='weight'):
+        if v == max_degree:
+            max_country = k
+            break
+    max_country_l.append(grafo.nodes.data("label")[max_country])
+    max_degree_l.append(max_degree)
+
+    min_degree = min(degree.values())
+    min_country = ''
+    for k, v in grafo.degree(weight='weight'):
+        if v == min_degree:
+            min_country = k
+            break
+    min_country_l.append(grafo.nodes.data("label")[min_country])
+    min_degree_l.append(min_degree)
+
+global_dict['mean_degree'] = mean_degree_l
+global_dict['max_country'] = max_country_l
+global_dict['max_degree'] = max_degree_l
+global_dict['min_country'] = min_country_l
+global_dict['min_degree'] = min_degree_l
+
+print(viral_dict)
+print(global_dict)
