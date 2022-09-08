cfcfc71a0dbb5472beb655d06dc2bdd139e8d7d2
mango <fedemarc24@gmail.com>
2022-09-08 17:47:29 +0200

    Added global centrality calculator script,modified databases

--- Network_Analyses/Eigen_Centrality.py
+++ Network_Analyses/Eigen_Centrality.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 import networkx as nx
 import pandas as pd
 import pathlib
@@ -41,21 +40,29 @@ for z in range(1, day+1):
         for i in eigen_con_pesi:
             eigen[i] = eigen_con_pesi[i] + eigen[i]
 
-eigen_top = dict(sorted(eigen.items(), key=lambda item: item[1] ,reverse=False)).values()
+eigen_top = eigen.values()
 df['eigen_top'] = eigen_top
 fig_top = px.choropleth(
     df, 
     locations='iso', 
     color='eigen_top',
-    projection='natural earth'
-    
+    projection='natural earth',
+    color_continuous_scale=['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58'] ,
+    scope = 'world',
 )
+
+
 fig_top.update_geos(
     visible=False, 
     resolution=50,
     showcountries=True, 
     countrycolor="White",
-    bgcolor='#191414'   
+    bgcolor='#1c1e21',   
+)
+fig_top.update_layout(
+    font_size = 18,
+    font_color = 'White',
+    paper_bgcolor = "#1c1e21",
 )
 fig_top.show()
 #----EIGEN-VIRAL----
@@ -76,13 +83,16 @@ for z in range(1, day+1):
             eigen[i] = eigen_con_pesi[i] + eigen[i]
 
 
-eigen_viral = dict(sorted(eigen.items(), key=lambda item: item[0] ,reverse=False)).values()
+eigen_viral = eigen.values()
 df['eigen_viral'] = eigen_viral
 fig_viral = px.choropleth(
     df, 
     locations='iso', 
-    color='eigen_viral',#eigenvector == autovettore
-    projection='natural earth'
+    color='eigen_viral',#eigenvector == autovettore,
+    color_continuous_scale=['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58'] ,
+    projection='natural earth',
+    scope = 'world',
+    labels='Grado Eigen Viral'
     
 )
 fig_viral.update_geos(
@@ -90,7 +100,13 @@ fig_viral.update_geos(
     resolution=50,
     showcountries=True, 
     countrycolor="White",
-    bgcolor='#191414'   
+    bgcolor='#1c1e21',
 )
-fig_viral.write_image("eigen_viral.png")
+fig_viral.update_layout(
+    font_size = 18,
+    font_color = 'White',   
+    paper_bgcolor = "#1c1e21",
+
+)
+fig_viral.write_image("eigen_viral.svg")
 fig_viral.show()
--- Network_Analyses/Glb_Centrality.py
+++ Network_Analyses/Glb_Centrality.py
@@ -1,5 +1,8 @@
 import csv
 import pathlib
+import pycountry
+import pandas as pd
+import plotly.express as px
 #https://extendsclass.com/merge-csv.html
 #use this file in order to merge all the global-centrality file
 
@@ -14,23 +17,86 @@ with open(data_path.joinpath('Data_Gathering/global.csv'), 'r') as file:
 
 
 #----TOP----
-with open(f'Dataset/global-centrality/global-centrality-merged.csv', 'r', encoding="utf-8") as file:
+
+with open(data_path.joinpath(f'Dataset/global-centrality/global-centrality-merged.csv'), 'r', encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         centrality[row['Country']] = centrality[row['Country']] + int(row['Centrality'])
 
 #global-centrality of top
-centrality_top = dict(sorted(centrality.items(), key=lambda item: item[1] ,reverse=True))
+centrality_top = centrality.values()
+countries = list()
+data_path = pathlib.Path(__file__).parent.absolute().parent
+with open(data_path.joinpath('Data_Gathering/global.csv'), 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        countries.append(row)
+df = pd.DataFrame(data=[country['Name'] for country in countries][1:],columns=['Name'])
+countr = {}
+for country in pycountry.countries:
+    countr[country.name.split(',')[0]] = country.alpha_3
+df['iso'] = [countr[country] for country in df['Name']]
+df['centrality_top'] = centrality_top
+
+fig_top = px.choropleth(
+    df, 
+    locations='iso', 
+    color='centrality_top',
+    projection='natural earth',
+    color_continuous_scale=['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58'] ,
+    scope = 'world',
+)
+
+
+fig_top.update_geos(
+    visible=False, 
+    resolution=50,
+    showcountries=True, 
+    countrycolor="White",
+    bgcolor='#1c1e21',   
+)
+fig_top.update_layout(
+    font_size = 18,
+    font_color = 'White',
+    paper_bgcolor = "#1c1e21",
+)
+#fig_top.show()
 
 
 #----VIRAL----
 for x in centrality:
     centrality[x] = 0
 
-with open(f'Dataset/viral-centrality/viral-centrality-merged.csv', 'r', encoding="utf-8") as file:
+with open(data_path.joinpath(f'Dataset/viral-centrality/viral-centrality-merged.csv'), 'r', encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         centrality[row['Country']] = centrality[row['Country']] + int(row['Centrality'])
 
 #global-centrality of viral
-centrality_viral = dict(sorted(centrality.items(), key=lambda item: item[1] ,reverse=True))
+centrality_viral = centrality.values()
+df['centrality_viral'] = centrality_viral
+
+
+fig_top = px.choropleth(
+    df, 
+    locations='iso', 
+    color='centrality_viral',
+    projection='natural earth',
+    color_continuous_scale=['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58'] ,
+    scope = 'world',
+)
+
+
+fig_top.update_geos(
+    visible=False, 
+    resolution=50,
+    showcountries=True, 
+    countrycolor="White",
+    bgcolor='#1c1e21',   
+)
+fig_top.update_layout(
+    font_size = 18,
+    font_color = 'White',
+    paper_bgcolor = "#1c1e21",
+)
+fig_top.show()
